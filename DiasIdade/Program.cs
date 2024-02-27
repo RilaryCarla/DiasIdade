a6f2beb5fd38252f451356f00b44892a9305d43a
Usuario <Usuario@CPD02-M02>
2024-02-27 11:52:05 -0300

    Adicionar arquivos de projeto.

---
+++ DiasIdade/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiasIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade: ");
+            int anos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite quantos meses se passaram desde o seu aniversário: ");
+            int meses = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite quantos dias se passam do seu aniversário ate completar o mês: ");
+            int dias = int.Parse(Console.ReadLine());
+
+            int IdadeAnos = anos * 365;
+            int IdadeMeses = meses * 30;
+            int Total = IdadeAnos + IdadeMeses + dias;
+            Console.WriteLine("A sua idade em dias é: " + Total);
+            Console.ReadKey();
+        }
+    }
+}
